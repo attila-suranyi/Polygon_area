e6a4fabdb23b96ccf93cc8cc5c8626f07a60d89a
Attila Surányi <attila.suranyi@protonmail.com>
2020-02-07 11:39:12 +0100

    initial backend commit

---
+++ polygon_backend/src/main/java/com/atis/polygon_area/PolygonAreaApplication.java
@@ -0,0 +1,13 @@
+package com.atis.polygon_area;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolygonAreaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolygonAreaApplication.class, args);
+    }
+
+}
