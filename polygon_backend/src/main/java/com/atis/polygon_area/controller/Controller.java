3764a21bf11961a251309d00c7c8a28bc331e32d
Attila Surányi <attila.suranyi@protonmail.com>
2020-02-11 12:36:37 +0100

    CrossOrigin annotation to controller

--- polygon_backend/src/main/java/com/atis/polygon_area/controller/Controller.java
+++ polygon_backend/src/main/java/com/atis/polygon_area/controller/Controller.java
@@ -5,6 +5,7 @@ import com.atis.polygon_area.geometry.Tetrahedron;
 import com.atis.polygon_area.geometry.Triangle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@CrossOrigin
 public class Controller {
 
     @Autowired
