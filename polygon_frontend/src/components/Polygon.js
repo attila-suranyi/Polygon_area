0292e55bf16345c37121d0c09a86291be4bcaefb
daniel-kalocsay <kdani782@gmail.com>
2020-02-13 17:05:34 +0100

    added basic skybox

--- polygon_frontend/src/components/Polygon.js
+++ polygon_frontend/src/components/Polygon.js
@@ -69,6 +69,7 @@ export default () => (
             gl.shadowMap.enabled = true;
             gl.shadowMap.type = THREE.PCFSoftShadowMap;
         }}
+        alpha={true}
     >
         <Polygon />
         <Plane />
