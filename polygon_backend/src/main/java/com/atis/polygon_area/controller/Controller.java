67399ffd89a03e1ee7040149d33a345e5f228eb2
Attila Surányi <attila.suranyi@protonmail.com>
2020-03-02 11:58:58 +0100

    user input route

--- polygon_backend/src/main/java/com/atis/polygon_area/controller/Controller.java
+++ polygon_backend/src/main/java/com/atis/polygon_area/controller/Controller.java
@@ -1,15 +1,17 @@
 package com.atis.polygon_area.controller;
 
+import com.atis.polygon_area.geometry.Polygon;
+import com.atis.polygon_area.geometry.Vertex;
 import com.atis.polygon_area.shapes.Icosahedron;
 import com.atis.polygon_area.shapes.Tetrahedron;
 import com.atis.polygon_area.shapes.Triangle;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -24,6 +26,10 @@ public class Controller {
 
     @Autowired
     private Icosahedron icosahedron;
+
+    @Qualifier("polygon")
+    @Autowired
+    private Polygon polygon;
 
     @GetMapping("/triangle")
     public ResponseEntity getTriangleGeometry() {
@@ -48,6 +54,18 @@ public class Controller {
         Map<Object, Object> model = new HashMap<>();
         model.put("area", icosahedron.getArea());
         model.put("triangles", icosahedron.getOrderedTriangleCoordinates());
+
+        return ResponseEntity.ok(model);
+    }
+
+    @PostMapping
+    public ResponseEntity userPolygon(@RequestBody List<Vertex> points) throws Exception {
+        points.forEach(polygon :: addVertex);
+        polygon.calculatePolygonGeometry();
+
+        Map<Object, Object> model = new HashMap<>();
+        model.put("area", polygon.getArea());
+        model.put("triangles", polygon.getOrderedTriangleCoordinates());
 
         return ResponseEntity.ok(model);
     }
